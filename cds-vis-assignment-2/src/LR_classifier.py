283c062dbbc3bacabe12f794218ce1d5fe7d6073
lrahbek <laura.rahbek@gmail.com>
2024-03-14 10:13:51 +0100

    commit lr classifier

--- cds-vis-assignment-2/src/LR_classifier.py
+++ cds-vis-assignment-2/src/LR_classifier.py
@@ -8,17 +8,29 @@ from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
 import argparse
 
-def outpath():
+def get_arguments():
     parser = argparse.ArgumentParser()
+
     parser.add_argument(
                         "--outpath",
-                       "-o", 
+                        "-o", 
                         required = False,
                         default = "../out/LR_classification_rep.txt",
-                        help="The input text file")
+                        help="The path for the classification report")
+
+    parser.add_argument(
+                        "--imageloader",
+                        "-i", 
+                        required = False,
+                        default = cifar10.load_data(),
+                        help= "The function used to load in X_train, y_train, X_test and y_test")
     
     args = parser.parse_args()
     return args
+
+def image_loader(load_fun):
+    (X_train, y_train), (X_test, y_test) = load_fun
+    return X_train, y_train, X_test, y_test
 
 
 def preprocess(images):
@@ -35,10 +47,10 @@ def preprocess(images):
     return preprocessed_images
 
 def LR_classifier(X_prep_train, y_train, X_prep_test, y_test, outpath):
-    y_train = np.ravel(y_train)
-    y_test = np.ravel(y_test)
+
     fitted_classifier = LogisticRegression(random_state=42).fit(X_prep_train, y_train)
     y_pred = fitted_classifier.predict(X_prep_test)
+
     metrics_rep = metrics.classification_report(y_test, y_pred)
     filepath_metrics = open(outpath, 'w')
     filepath_metrics.write(metrics_rep)
@@ -46,14 +58,11 @@ def LR_classifier(X_prep_train, y_train, X_prep_test, y_test, outpath):
 
 
 def main():
-    args = outpath()
-
-    (X_train, y_train), (X_test, y_test) = cifar10.load_data()
-
+    args = get_arguments()
+    X_train, y_train, X_test, y_test = image_loader(args.imageloader)
     X_train_pre = preprocess(X_train)
     X_test_pre = preprocess(X_test)
-
-    LR_classifier(X_prep_train, y_train, X_prep_test, y_test, args.outpath)
+    LR_classifier(X_train_pre, y_train, X_test_pre, y_test, args.outpath)
     
 
 if __name__ == "__main__":
